2071c785487fdadfa50ffd33b8beffdf7820c866
AbideenAyodimeji <abideenoygbenro2018@gmail.com>
2023-09-04 18:01:23 -0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import LoginScreen from './Authentication/Login'
+import RegisterScreen from './Authentication/Register'
+import DashBoard from './Authentication/DashBoard'
+import WelcomeScreen from './Authentication/Welcome'
+
+const Stack = createNativeStackNavigator()
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Login'
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+        <Stack.Screen name='DashBoard' component={DashBoard} />
+        <Stack.Screen
+          name='Welcome'
+          component={WelcomeScreen}
+          options={{ headerShown: true }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
